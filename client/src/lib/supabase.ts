6e386dd5212e72e8968f3fef57ee204d9402edde
Misso Misso <laurentmisso@gmail.com>
2026-02-11 22:59:56 +0800

    More Edits..

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
